137f8890a2e2f701f9d52bb12a0833a0167aac87
Uday Pratap Singh <uday.singh@tothenew.com>
2016-03-28 17:25:57 +0530

    Implemented contact edit with jquery text, index, val, hide, reseting values tr td eq and modal show

--- js/app.js
+++ js/app.js
@@ -1,26 +1,52 @@
-var contacts = [];
-
 $(document).ready(function () {
     $("#addContact").submit(function () {
         var name = $("#name").val();
         var email = $("#email").val();
         var phoneNumber = $("#phoneNumber").val();
         var company = $("#company").val();
-        var contact = {name: name, email: email, phoneNumber: phoneNumber, company: company};
-        contacts.push(contact);
+        var index = $("#index").val();
+        var contact = {name: name, email: email, phoneNumber: phoneNumber, company: company, index: index};
         show(contact);
         $("#createModal").modal('hide');
+        return false;
     })
 });
 
 function show(contact) {
-    $('.noData').remove();
-    var data = "<tr><td>";
-    data = data + contact.name + "</td><td>";
-    data = data + contact.email + "</td><td>";
-    data = data + contact.phoneNumber + "</td><td>";
-    data = data + contact.company + "</td>";
-    data = data + "<td><a href='javascript:void(0)' data-toggle='modal' data-target='#createModal'><i class='fa fa-edit'></i>&nbsp;</a> <a href='javascript:void(0)'>" +
-        " <i class='fa fa-trash'></i></a></td></tr>";
-    $("#contacts tbody").append(data);
+    $('.noData').hide();
+    if (parseInt(contact.index) >= 0) {
+        $("#contacts tbody tr:eq(" + contact.index + ") td:eq(0)").text(contact.name);
+        $("#contacts tbody tr:eq(" + contact.index + ") td:eq(1)").text(contact.email);
+        $("#contacts tbody tr:eq(" + contact.index + ") td:eq(2)").text(contact.phoneNumber);
+        $("#contacts tbody tr:eq(" + contact.index + ") td:eq(3)").text(contact.company);
+    } else {
+        var data = "<tr><td>";
+        data = data + contact.name + "</td><td>";
+        data = data + contact.email + "</td><td>";
+        data = data + contact.phoneNumber + "</td><td>";
+        data = data + contact.company + "</td>";
+        data = data + "<td><a href='javascript:void(0)' onclick='edit(this)'><i class='fa fa-edit'></i>&nbsp;</a> <a href='javascript:void(0)'>" +
+            " <i class='fa fa-trash'></i></a></td></tr>";
+        $("#contacts tbody").append(data);
+    }
+}
+
+function edit(btn) {
+    var $row = $(btn).parent().parent();
+    var index = $row.index();
+    var name = $row.find('td:eq(0)').text();
+    var email = $row.find('td:eq(1)').text();
+    var phoneNumber = $row.find('td:eq(2)').text();
+    var company = $row.find('td:eq(3)').text();
+    $("#name").val(name);
+    $("#email").val(email);
+    $("#phoneNumber").val(phoneNumber);
+    $("#company").val(company);
+    $("#index").val(index);
+    $("#createModal").modal('show');
+}
+
+function create() {
+    $("#addContact").find("input[type=text], input[type=email]").val("");
+    $("#createModal").modal('show');
 }
